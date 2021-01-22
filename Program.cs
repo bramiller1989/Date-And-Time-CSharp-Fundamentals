b2e74ff54c3fa5bc50659162729506780a8a591d
bramiller1989 <bramiller1989@gmail.com>
2021-01-21 18:35:10 -0600

    print long and short date and time

--- Program.cs
+++ Program.cs
@@ -16,7 +16,11 @@ namespace DateAndTime
             var tomorrow = now.AddDays(1);
             var yesterday = now.AddDays(-1);
 
-
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString());
         }
     }
 }
