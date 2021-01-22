ab83ff8694be84413c9ce38e1418454ece221122
bramiller1989 <bramiller1989@gmail.com>
2021-01-21 18:32:06 -0600

    print time

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace DateAndTime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dateTime = new DateTime(2015, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            Console.WriteLine("Hour: " + now.Hour);
+            Console.WriteLine("Minute: " + now.Minute);
+
         }
     }
 }
