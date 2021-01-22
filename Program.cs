d184cefb7feac05748123bf71e43b9b39cfe0e02
bramiller1989 <bramiller1989@gmail.com>
2021-01-21 18:42:00 -0600

    add timespan

--- Program.cs
+++ Program.cs
@@ -21,6 +21,20 @@ namespace DateAndTime
             Console.WriteLine(now.ToLongTimeString());
             Console.WriteLine(now.ToShortTimeString());
             Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+
+            var timeSpan = new TimeSpan(1, 2, 3);
+            Console.WriteLine("timeSpan: " + timeSpan);
+
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
+            Console.WriteLine("timeSpan1: " + timeSpan1);
+            Console.WriteLine("timeSpan2: " + timeSpan2);
+
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
         }
     }
 }
